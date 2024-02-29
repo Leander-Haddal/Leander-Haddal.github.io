14e8e4c5135147a56e93ed732a828ec3ba9d32ba
Leander-Haddal <122444622+Leander-Haddal@users.noreply.github.com>
2024-02-29 14:39:09 +0100

    magnet with each movie and play update

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ fetch('movies.json')
       movieDiv.className = 'movie';
 
       const a = document.createElement('a');
-      a.href = movie.url; // Set the href to the movie's URL
+      a.href = `play.html?magnet=${encodeURIComponent(movie.magnet)}`; // Set the href to play.html with the magnet link
 
       const img = document.createElement('img');
       img.src = movie.picture;
