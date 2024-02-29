2dc9f7f292a99ca0c30c5071d760a72133f59867
Leander-Haddal <122444622+Leander-Haddal@users.noreply.github.com>
2024-02-29 16:03:06 +0100

    something i guess added back copypase player as play2

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ fetch('movies.json')
       movieDiv.className = 'movie';
 
       const a = document.createElement('a');
-      a.href = `play.html?magnet=${encodeURIComponent(movie.magnet)}`; // Set the href to play.html with the magnet link
+      a.href = `play.html?title=${encodeURIComponent(movie.title)}`; // Set the href to play.html with the title
 
       const img = document.createElement('img');
       img.src = movie.picture;
